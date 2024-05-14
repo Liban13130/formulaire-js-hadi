d72fad6009ac60238aa5774ef281278291de7066
Nabil Hellal <nabil-13130@hotmail.fr>
2024-05-14 12:02:18 +0200

    fini

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
 const form = document.querySelector("form");
 const inputMail = document.getElementById("email")
+const errorMsg = document.getElementById("error")
+const userMail = document.getElementById("user-mail")
+const landingPage = document.querySelector(".container")
+const subscriptionPage = document.querySelector(".container-2")
+const backHome = document.querySelector('.back-home')
+const regexEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
+
 
 
 form.addEventListener("submit", (e) => {
     e.preventDefault()
 
-    console.log(inputMail.value);
+    if(regexEmail.test(inputMail.value)){
+        errorMsg.style.display = "none"
+        inputMail.style.borderColor = "inherit"
+        subscriptionPage.style.display = "flex"
+        landingPage.style.display = "none"
+
+        userMail.textContent = inputMail.value
+        userMail.style.fontWeight = "bold"
+    }else{
+        inputMail.style.borderColor = "red"
+        errorMsg.style.display = "block"
+    }
+})
+
+backHome.addEventListener('click', () => {
+    subscriptionPage.style.display = "none"
+    landingPage.style.display = "flex"
+    inputMail.value = ""
 })
